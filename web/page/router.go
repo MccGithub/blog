43cc62a99ca46ee9b6e1324cf71add68cbab7b0a
mcc <ma.cc@qq.com>
2019-10-25 16:29:38 +0800

    将/路由直接交给Article处理

--- web/page/router.go
+++ web/page/router.go
@@ -5,7 +5,8 @@ import "github.com/go-chi/chi"
 func Router() chi.Router {
 	router := chi.NewRouter()
 
-	router.Get("/", Home)
+	router.Get("/", Article)
+	router.Get("/article", Article)
 
 	return router
 }
