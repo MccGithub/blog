81b25b264076f951aa33f43c56038f3514e2efca
mcc <ma.cc@qq.com>
2019-10-25 10:42:34 +0800

    完善main,添加获取main文件绝对路径的方法

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -2,21 +2,39 @@ package main
 
 import (
 	"flag"
+	"github.com/MccGithub/blog/util"
 	"github.com/MccGithub/blog/web"
 	"github.com/sirupsen/logrus"
+	"os"
+	"runtime"
+	"strings"
 )
 
 var (
+	logLevel string
 	opt web.Opt
 )
 
 func init() {
-	flag.StringVar(&opt.Address, "listen", ":8080", "listening address")
+	flag.StringVar(&opt.Address, "listen", ":80", "listening address")
+	flag.StringVar(&logLevel, "log.level", "warn", "logging level: trace, debug, info, error, fatal")
 
 	flag.Parse()
 }
 
+func getCurrent() {
+	_, util.TmplBasePath, _, _ = runtime.Caller(0)
+	i := strings.LastIndex(util.TmplBasePath, string(os.PathSeparator))
+	util.TmplBasePath = util.TmplBasePath[0:i+1]
+}
+
 func main() {
+	getCurrent()
+	if lvl, err := logrus.ParseLevel(logLevel); err != nil {
+		logrus.Fatal(err)
+	} else {
+		logrus.SetLevel(lvl)
+	}
 	if err := web.Serve(opt); err != nil {
 		panic(err)
 	}
--- web/router.go
+++ web/router.go
@@ -3,6 +3,7 @@ package web
 import (
 	"fmt"
 	"github.com/MccGithub/blog/web/api"
+	"github.com/MccGithub/blog/web/page"
 	"github.com/go-chi/chi"
 	"net/http"
 )
@@ -11,9 +12,14 @@ type Opt struct {
 	Address string
 }
 
+func home(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func Serve(opt Opt) error {
 	handler := chi.NewRouter()
 
+	handler.Mount("/", page.Router())
 	handler.Mount("/api", api.Router())
 
 	fmt.Println("Listening at ", opt.Address)
