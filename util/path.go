0dd01e91ed760bc30fbda2419940f3e0b5bdda0d
mcc <ma.cc@qq.com>
2019-10-25 10:51:15 +0800

    用于获取模板文件夹绝对路径和根据模板显示HTML页面,需先在main中设置两个Path变量

--- util/path.go
+++ util/path.go
@@ -1,1 +1,25 @@
 package util
+
+import (
+	"html/template"
+	"io"
+	"path/filepath"
+)
+
+var (
+	TmplBasePath string
+	TmplRelativePath string
+)
+
+func GetTempPath(file string) string {
+	return filepath.Join(TmplBasePath, TmplRelativePath, file)
+}
+
+func View(file string, w io.Writer, data interface{}) error {
+	tmpl, err := template.ParseFiles(GetTempPath(file))
+	if err != nil {
+		return err
+	}
+
+	return tmpl.Execute(w, data)
+}
