ee64f075806948d42294354f316b7d474b54c4a8
mcc <ma.cc@qq.com>
2019-10-25 10:44:46 +0800

    用于展示页面的方法集合

--- web/page/page.go
+++ web/page/page.go
@@ -1,1 +1,13 @@
 package page
+
+import (
+	"github.com/MccGithub/blog/util"
+	"github.com/sirupsen/logrus"
+	"net/http"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	if err := util.View("html/main.html", w, nil); err != nil {
+		logrus.Warn(err)
+	}
+}
