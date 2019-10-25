7f7b5e13afe8d73de214b400155f8e649be1c96a
mcc <ma.cc@qq.com>
2019-10-25 12:10:57 +0800

    修改View参数列表以支持传入多个模板路径正确解析子模板

--- util/path.go
+++ util/path.go
@@ -7,19 +7,38 @@ import (
 )
 
 var (
-	TmplBasePath string
+	BasePath         string
 	TmplRelativePath string
+	ArtiRelativePath string
 )
 
+type Data map[string]interface{}
+
 func GetTempPath(file string) string {
-	return filepath.Join(TmplBasePath, TmplRelativePath, file)
+	return filepath.Join(BasePath, TmplRelativePath, file)
 }
 
-func View(file string, w io.Writer, data interface{}) error {
-	tmpl, err := template.ParseFiles(GetTempPath(file))
+func GetArtiPath(file string) string {
+	return filepath.Join(BasePath, ArtiRelativePath, file)
+}
+
+func View(w io.Writer, data Data, file ...string) error {
+	for i, v := range file {
+		file[i] = GetTempPath(v)
+	}
+	tmpl, err := template.ParseFiles(file...)
 	if err != nil {
 		return err
 	}
 
 	return tmpl.Execute(w, data)
 }
+//模板嵌套时需要传入母子模板多个文件路径解析,故修改参数列表以支持传入多个文件路径参数
+//func View(file string, w io.Writer, data Data) error {
+//	tmpl, err := template.ParseFiles(GetTempPath(file))
+//	if err != nil {
+//		return err
+//	}
+//
+//	return tmpl.Execute(w, data)
+//}
