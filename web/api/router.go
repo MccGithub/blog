6692abc6a3c0a907e3c619008ab5b7661571eae8
mcc <ma.cc@qq.com>
2019-10-25 10:43:24 +0800

    完善roter

--- web/api/router.go
+++ web/api/router.go
@@ -5,7 +5,7 @@ import "github.com/go-chi/chi"
 func Router() chi.Router {
 	router := chi.NewRouter()
 
-	router.Get("/xxx", Xxx)
+	//router.Get("/xxx", Xxx)
 
 	return router
 }
--- web/page/router.go
+++ web/page/router.go
@@ -1,1 +1,11 @@
 package page
+
+import "github.com/go-chi/chi"
+
+func Router() chi.Router {
+	router := chi.NewRouter()
+
+	router.Get("/", Home)
+
+	return router
+}
