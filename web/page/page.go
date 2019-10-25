9678c01146a61e3faeff043cbaf602a935124f8e
mcc <ma.cc@qq.com>
2019-10-25 16:29:09 +0800

    删掉Home方法,直接交给Article处理

--- web/page/page.go
+++ web/page/page.go
@@ -6,8 +6,34 @@ import (
 	"net/http"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
-	if err := util.View(w, nil, "html/main.html", "html/article.html"); err != nil {
+func Article(w http.ResponseWriter, r *http.Request) {
+	// 获取存放文章文件夹中的文件信息
+	filesInfo, err := util.GetFileListByPath(util.GetArtiBasePath())
+	if err != nil {
+		logrus.Warn(err)
+	}
+
+	// 提取出文件名存放在filesNames中
+	var filesNames []string
+	for _, v := range filesInfo {
+		if !v.IsDir() {
+			filesNames = append(filesNames, v.Name())
+		}
+	}
+
+	// 初始化模板变量, 设置文件名列表
+	data := util.Data{
+		"files": filesNames,
+	}
+
+	// 读取url中的 file 参数值
+	fileName := r.FormValue("file")
+	if fileName == "" {
+		fileName = "welcome.txt"
+	}
+	data["file"] = fileName
+
+	if err := util.View(w, data, "html/main.html", "html/article.html"); err != nil {
 		logrus.Warn(err)
 	}
 }
