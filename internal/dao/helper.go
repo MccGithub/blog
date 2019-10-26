01efe80daf494cf57feef059fa64d0387d5e8e2c
mcc <ma.cc@qq.com>
2019-10-26 14:47:17 +0800

    数据库helper

--- internal/dao/helper.go
+++ internal/dao/helper.go
@@ -1,1 +1,41 @@
 package dao
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/sirupsen/logrus"
+)
+
+type SQLHelper struct {
+	db *sql.DB
+}
+
+func NewSQLHelper(driver, connstr string) (*SQLHelper, error) {
+	db, err := sql.Open(driver, connstr)
+	if err != nil {
+		return nil, err
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = tx.Exec(`CREATE TABLE IF NOT EXISTS article (
+			id varchar(128),
+			name varchar(128) NOT NULL,
+			author varchar(128) NOT NULL,
+			brief TEXT NOT NULL,
+			content TEXT NOT NULL,
+			PRIMARY KEY (id)
+			)`)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = tx.Commit(); err != nil {
+		return nil, err
+	}
+	logrus.Debug("database initialized")
+	return &SQLHelper{db:db}, nil
+}
