e6072023a456fe96c170b439e34dede3637737b4
mcc <ma.cc@qq.com>
2019-10-26 14:46:12 +0800

    文章存储结构(M层

--- internal/dao/article.go
+++ internal/dao/article.go
@@ -1,1 +1,22 @@
 package dao
+
+type Article struct {
+	helper SQLHelper
+
+	Id 		string `json:"id"`
+	Name 	string `json:"name"`
+	Brief 	string `json:"brief"`
+	Content string `json:"content"`
+	Author 	string `json:"author"`
+}
+
+func (helper SQLHelper) GetArticle(id string) *Article {
+	return &Article{
+		helper:  helper,
+		Id:      id,
+	}
+}
+
+func (article *Article) Get() error {
+	cmd := "SELECT "
+}
