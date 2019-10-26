7c513df869786d510b22a73db3c8bfc2e55dc9c0
mcc <ma.cc@qq.com>
2019-10-26 14:42:16 +0800

    重新规划路由, 分为主页和文章详情页

--- web/router.go
+++ web/router.go
@@ -2,14 +2,18 @@ package web
 
 import (
 	"fmt"
-	"github.com/MccGithub/blog/web/api"
-	"github.com/MccGithub/blog/web/page"
+	"github.com/MccGithub/blog/web/details"
+	"github.com/MccGithub/blog/web/index"
 	"github.com/go-chi/chi"
+	"github.com/sirupsen/logrus"
 	"net/http"
 )
 
 type Opt struct {
-	Address string
+	Address		string
+
+	DBDriver 	string
+	DBConn		string
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -19,8 +23,10 @@ func home(w http.ResponseWriter, r *http.Request) {
 func Serve(opt Opt) error {
 	handler := chi.NewRouter()
 
-	handler.Mount("/", page.Router())
-	handler.Mount("/api", api.Router())
+	logrus.Tracef("%+v", opt)
+
+	handler.Mount("/", index.Router())
+	handler.Mount("/details", details.Router())
 
 	fmt.Println("Listening at ", opt.Address)
 	return http.ListenAndServe(opt.Address, handler)
