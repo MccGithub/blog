ea31647862b411b53d7feb85156d5233bd2d816d
mcc <ma.cc@qq.com>
2019-10-26 18:15:36 +0800

    模拟数据测试模板日否正常

--- web/details/details.go
+++ web/details/details.go
@@ -1,1 +1,31 @@
 package details
+
+import (
+	"github.com/MccGithub/blog/util"
+	"github.com/sirupsen/logrus"
+	"net/http"
+)
+
+func Details(w http.ResponseWriter, r *http.Request) {
+	tmplFiles := []string{
+		"html/index.html",
+		"html/articles.html",
+		"html/details.html",
+	}
+	// 传入模拟数据以测试, 第一次是空数据
+	test_data := util.Data{
+		"target": "details",
+		"name": "文章详情页测试",
+		"author": "作者当然是我啦",
+		"content": `测试内容就是乱七八糟%>_<%<br />
+jsaiofhhaoihfa<br />
+叫啥佛我后爱疯hi哈搜发完后发动哈佛按时非 哈市豆腐<br />
+if哈哈是佛我荷藕我更好歌手阿福<br />
+后端和覅ua<br />`,
+	}
+	//if err := util.View(w, test_data, "html/index.html", "html/articles.html", "html/details.html"); err != nil {
+	if err := util.View(w, test_data, tmplFiles...); err != nil {
+		logrus.Warn(err)
+	}
+
+}
--- web/index/index.go
+++ web/index/index.go
@@ -3,41 +3,56 @@ package index
 import (
 	"github.com/MccGithub/blog/util"
 	"github.com/sirupsen/logrus"
-	"io/ioutil"
 	"net/http"
-	"strings"
 )
 
+// 这是文章存储为txt文件时的实现
+//func Articles(w http.ResponseWriter, r *http.Request) {
+//	// 获取存放文章文件夹中的文件信息
+//	filesInfo, err := util.GetFileListByPath(util.GetArtiBasePath())
+//	if err != nil {
+//		logrus.Warn(err)
+//	}
+//
+//	// 提取出文件名(去掉后缀名后)存放在filesNames中
+//	var filesNames []string
+//	for _, v := range filesInfo {
+//		if !v.IsDir() {
+//			filesNames = append(filesNames, v.Name()[0:strings.LastIndex(v.Name(), ".")])
+//		}
+//	}
+//
+//	// 初始化模板变量, 设置文件名列表
+//	data := util.Data{
+//		"files": filesNames,
+//	}
+//
+//	// 读取url中的 file 参数值
+//	fileName := r.FormValue("file")
+//	if fileName == "" {
+//		fileName = "welcome"
+//	}
+//	data["file"] = fileName
+//	content, err := ioutil.ReadFile(util.GetArtiPath(data["file"].(string))+".txt")
+//	data["content"] = string(content)
+//
+//	if err := util.View(w, data, "html/index.html", "html/details.html"); err != nil {
+//		logrus.Warn(err)
+//	}
+//}
 func Articles(w http.ResponseWriter, r *http.Request) {
-	// 获取存放文章文件夹中的文件信息
-	filesInfo, err := util.GetFileListByPath(util.GetArtiBasePath())
-	if err != nil {
+	tmplFiles := []string{
+		"html/index.html",
+		"html/articles.html",
+		"html/details.html",
+	}
+	// 传入模拟数据以测试, 第一次是空数据
+	test_data := util.Data{
+		"target": "index",
+	}
+	//if err := util.View(w, test_data, "html/index.html", "html/articles.html", "html/details.html"); err != nil {
+	if err := util.View(w, test_data, tmplFiles...); err != nil {
 		logrus.Warn(err)
 	}
 
-	// 提取出文件名(去掉后缀名后)存放在filesNames中
-	var filesNames []string
-	for _, v := range filesInfo {
-		if !v.IsDir() {
-			filesNames = append(filesNames, v.Name()[0:strings.LastIndex(v.Name(), ".")])
-		}
-	}
-
-	// 初始化模板变量, 设置文件名列表
-	data := util.Data{
-		"files": filesNames,
-	}
-
-	// 读取url中的 file 参数值
-	fileName := r.FormValue("file")
-	if fileName == "" {
-		fileName = "welcome"
-	}
-	data["file"] = fileName
-	content, err := ioutil.ReadFile(util.GetArtiPath(data["file"].(string))+".txt")
-	data["content"] = string(content)
-
-	if err := util.View(w, data, "html/main.html", "html/article.html"); err != nil {
-		logrus.Warn(err)
-	}
 }
