d6c366c22389465f1d3fcf89a83f6e0d8faafbc2
mcc <ma.cc@qq.com>
2019-10-27 11:31:12 +0800

    添加DBHandler中间件

--- web/router.go
+++ web/router.go
@@ -2,6 +2,7 @@ package web
 
 import (
 	"fmt"
+	"github.com/MccGithub/blog/internal/dao"
 	"github.com/MccGithub/blog/web/details"
 	"github.com/MccGithub/blog/web/index"
 	"github.com/go-chi/chi"
@@ -24,6 +25,10 @@ func Serve(opt Opt) error {
 	handler := chi.NewRouter()
 
 	logrus.Tracef("%+v", opt)
+	if opt.DBConn != "" || opt.DBDriver != "" {
+		logrus.Trace("set db")
+		handler.Use(dao.DBHandler(opt.DBDriver, opt.DBConn))
+	}
 
 	handler.Mount("/", index.Router())
 	handler.Mount("/details", details.Router())
