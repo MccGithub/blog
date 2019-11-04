d9f2430b0c3dd5a37e356e7f93e76423b09f863a
mcc <ma.cc@qq.com>
2019-11-04 21:39:14 +0800

    添加timestamp字段

--- internal/dao/article.go
+++ internal/dao/article.go
@@ -1,5 +1,7 @@
 package dao
 
+import "errors"
+
 type Article struct {
 	helper SQLHelper
 
@@ -8,6 +10,7 @@ type Article struct {
 	Brief 	string `json:"brief"`
 	Content string `json:"content"`
 	Author 	string `json:"author"`
+	Time 	int64
 }
 
 func (helper SQLHelper) GetArticle(id string) *Article {
@@ -17,22 +20,47 @@ func (helper SQLHelper) GetArticle(id string) *Article {
 	}
 }
 
+func (helper SQLHelper) TraversingArticles() ([]Article, error) {
+	var articles []Article
+	//cmd := "SELECT id, name, author, brief, content FROM article ORDER BY MODIFYTIME DESC"
+	cmd := "SELECT id, name, author, brief, content, timestamp FROM article ORDER BY timestamp DESC"
+	rows, err := helper.db.Query(cmd)
+	if err != nil {
+		return nil, err
+	}
+	for rows.Next() {
+		article := &Article{
+			helper:  helper,
+		}
+		if err = rows.Scan(&article.Id, &article.Name, &article.Author, &article.Brief, &article.Content, &article.Time); err != nil {
+			return nil, err
+		}
+		articles = append(articles, *article)
+	}
+	return articles, nil
+}
+
 func (article *Article) Get() error {
-	cmd := "SELECT name, author, brief, content FROM article WHERE id = ?"
+	count := 0
+	cmd := "SELECT name, author, brief, content, timestamp FROM article WHERE id = ?"
 	rows, err := article.helper.db.Query(cmd, article.Id)
 	if err != nil {
 		return err
 	}
 	for rows.Next() {
-		if err = rows.Scan(&article.Name, &article.Author, &article.Brief, &article.Content); err != nil {
+		count++
+		if err = rows.Scan(&article.Name, &article.Author, &article.Brief, &article.Content, &article.Time); err != nil {
 			return err
 		}
+	}
+	if count == 0 {
+		return errors.New("Did not find the data with id " + article.Id + ".")
 	}
 	return err
 }
 
 func (article *Article) Insert() error {
-	cmd := "INSERT INTO article(id, name, author, brief, content) VALUES(?, ?, ?, ?, ?)"
-	_, err := article.helper.db.Exec(cmd, article.Id, article.Name, article.Author, article.Brief, article.Content)
+	cmd := "INSERT INTO article(id, name, author, brief, content, timestamp ) VALUES(?, ?, ?, ?, ?, ?)"
+	_, err := article.helper.db.Exec(cmd, article.Id, article.Name, article.Author, article.Brief, article.Content, article.Time)
 	return err
 }
